b2976f08f8b3aac495db1f72376f98b9e9c6daca
thao <thaont124@outlook.com.vn>
2023-05-07 22:17:04 +0700

    thao

--- src/main/java/film/api/controller/ActorController.java
+++ src/main/java/film/api/controller/ActorController.java
@@ -13,20 +13,20 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/actor")
+@RequestMapping(value = "/ApiV1", produces = "application/json")
 public class ActorController {
 
     @Autowired
     private ActorService actorService;
 
-    @GetMapping("/list")
+    @GetMapping("/AllActor")
     public ResponseEntity<?> getAllActors() {
         String nhi = "toi te";
 
         return new ResponseEntity<>(actorService.getList(), HttpStatus.OK);
     }
 
-    @PostMapping("add")
+    @PostMapping("AllActor")
     public ResponseEntity<?> addActor(@RequestBody ActorDTO actorPost) {
 
         Actor savedActor = actorService.addActor(Actor.builder()
@@ -37,7 +37,7 @@ public class ActorController {
         return new ResponseEntity<>(savedActor, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/ActorByID/{id}")
     public ResponseEntity<?> getActorById(@PathVariable Long id) {
         Optional<Actor> actor = actorService.findById(id);
 
@@ -47,20 +47,28 @@ public class ActorController {
         return new ResponseEntity<>(new ActorDTO(actor.get()), HttpStatus.OK);
     }
 
-//    @PutMapping("/update/{id}")
-//    public ResponseEntity<Object> updateActor(@PathVariable Long id, @RequestBody ActorDTO updateActor) {
-//        Optional<Actor> actor = actorService.findById(id);
-//        if (!actor.isPresent()) {
-//            return new ResponseEntity<>("Không tồn tại có id = " + id, HttpStatus.OK);
-//        }
-//        actorService.updateActor(actor.get(), updateActor);
-//        return new ResponseEntity<>(actor, HttpStatus.OK);
-//    }
-//
-//    @DeleteMapping("/delete/{id}")
-//    public ResponseEntity<Object> deleteActor(@PathVariable Long id) {
-//        actorRepository.deleteById(id);
-//        return new ResponseEntity<>("Xóa thành công", HttpStatus.OK);
-//    }
+    @PatchMapping("/ActorByID/{ActorByID}")
+    public ResponseEntity<Object> updateActor(@PathVariable("ActorByID") Long id, @RequestBody ActorDTO actorPost) {
+        Actor updateActor = actorService.addActor(Actor.builder()
+                .ActorName(actorPost.getActorName())
+                .Age(actorPost.getAge())
+                .nativeLand(actorPost.getNativeLand())
+                .Sex(actorPost.getSex()).build());
+        Actor actor = actorService.updateActor(id, updateActor);
+        return new ResponseEntity<>(actor, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/ActorByID/{ActorByID}")
+    public ResponseEntity<?> deleteActor(@PathVariable Long ActorByID) {
+        actorService.deleteById(ActorByID);
+        return new ResponseEntity<>("Xóa thành công", HttpStatus.OK);
+    }
+
+    @GetMapping("/ActorByName/{ActorName}")
+    public ResponseEntity<List<Actor>> searchActors(@PathVariable("ActorName") String actorName) {
+        List<Actor> actors = actorService.searchActors(actorName);
+        return new ResponseEntity<>(actors, HttpStatus.OK);
+    }
+
 }
 
