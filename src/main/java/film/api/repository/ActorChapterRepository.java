25c0ac2bdb70a5cc1f3013ccc058723dae433a75
thao <thaont124@outlook.com.vn>
2023-05-07 21:02:58 +0700

    toi

--- src/main/java/film/api/repository/ActorChapterRepository.java
+++ src/main/java/film/api/repository/ActorChapterRepository.java
@@ -4,4 +4,10 @@ import film.api.models.ActorChapter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ActorChapterRepository extends JpaRepository<ActorChapter, Long> {
+    @Query("SELECT ac.Chapter FROM ActorChapter ac WHERE ac.Actor.id = :actorId")
+    List<Chapter> findChaptersByActorId(@Param("actorId") Long actorId);
+
+
+
+
 }
